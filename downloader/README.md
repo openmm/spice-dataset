41d9f5e8e37c93c2149a2c17ca56ba926be9a4ee
Peter Eastman <peastman@stanford.edu>
2022-05-30 14:19:56 -0700

    Downloader computes formation energy (#25)

--- downloader/README.md
+++ downloader/README.md
@@ -4,11 +4,11 @@ The full dataset is available on [QCArchive](https://qcarchive.molssi.org/).  It
 
 To run it, you first need to install a few libraries that it requires.  Use this command.
 
-    conda install -c conda-forge qcportal pyyaml h5py numpy
+    conda install -c conda-forge qcportal pyyaml h5py rdkit numpy
 
 Next edit the `config.yaml` file.  Select the data subsets to download (most often you will want all of them) and data values to include in the output file (`'DFT TOTAL ENERGY'` and `'DFT TOTAL GRADIENT'` are the most common ones; others are only needed for special purposes).  Comment out any subset or value you do not want by placing a `#` at the start of the line.
 
-Finally run `python downloader.py` to download the data.  This can take a few hours.  The data is stored in a HDF5 file called `SPICE.hdf5`.  It is structured as follows.
+Finally run `python downloader.py` to download the data.  This can take several hours.  The data is stored in a HDF5 file called `SPICE.hdf5`.  It is structured as follows.
 
 - There is one top level group for each unique molecule or cluster.  The name of each group is either a PubChem Substance ID (for PubChem molecules), an amino acid sequence (for dipeptides and solvated amino acids), or a SMILES string (for everything else).
 - Each group contains the following datasets.  `N` is the number of atoms in the molecule and `M` is the number of conformations.
@@ -16,5 +16,6 @@ Finally run `python downloader.py` to download the data.  This can take a few ho
   - `smiles`: The canonical SMILES string for the molecule.  It includes explicit hydrogens and atom indices.
   - `atomic_numbers`: Array of length `N` containing the atomic number of every atom.  They are ordered following the indices in the SMILES string.
   - `conformations`: Array of shape `(M, N, 3)` containing the atomic coordinates for every conformation.
+  - `formation_energy`: Array of length `M` containing the total energy of each conformation, minus the reference energies of the individual atoms when infinitely separated.  This is the most useful energy for most purposes, since it contains all energy components that vary with atom positions but removes the large constant part corresponding to the internal energies of individual atoms.
   - Other values as specified in the configuration file.  For example, `dft_total_energy` is an array of length `M` containing the energy of each conformation.  `dft_total_gradient` is an array of shape `(M, N, 3)` containing the gradient of the energy with respect to the atomic coordinates.
 - All values are in atomic units.  Distances are in bohr and energies in hartree.
--- downloader/downloader.py
+++ downloader/downloader.py
@@ -1,8 +1,19 @@
 from qcportal import FractalClient
 from collections import defaultdict
+from rdkit import Chem
 import numpy as np
 import h5py
 import yaml
+
+# Reference energies computed with Psi4 1.5 wB97M-D3BJ/def2-TZVPPD.
+
+atom_energy = {('Br', -1): -2574.2451510945853, ('Br', 0): -2574.1167240829964, ('C', -1): -37.91424135791358, ('C', 0): -37.87264507233593,
+               ('C', 1): -37.45349214963933, ('Ca', 2): -676.9528465198214, ('Cl', -1): -460.3350243496703, ('Cl', 0): -460.1988762285739,
+               ('F', -1): -99.91298732343974, ('F', 0): -99.78611622985483, ('H', 0): -0.4987605100487341, ('I', -1): -297.8813829975981,
+               ('I', 0): -297.76228914445625, ('K', 1): -599.8025677513111, ('Li', 1): -7.285254714046546, ('Mg', 2): -199.2688420040449,
+               ('N', -1): -54.602291095426494, ('N', 0): -54.62327513368922, ('N', 1): -54.08594142587869, ('Na', 1): -162.11366478783253,
+               ('O', -1): -75.17101657391741, ('O', 0): -75.11317840410095, ('O', 1): -74.60241514396725, ('P', 0): -341.3059197024934,
+               ('P', 1): -340.9258392474849, ('S', -1): -398.2405387031612, ('S', 0): -398.1599636677874, ('S', 1): -397.7746615977658}
 
 with open('config.yaml') as input:
     config = yaml.safe_load(input.read())
@@ -20,7 +31,7 @@ for subset in config['subsets']:
     mols_by_name = defaultdict(list)
     for i in range(len(recs)):
         rec = recs.iloc[i].record
-        if rec.status == 'COMPLETE':
+        if rec is not None and rec.status == 'COMPLETE':
             index = recs.index[i]
             name = index[:index.rfind('-')]
             recs_by_name[name].append(rec)
@@ -32,11 +43,15 @@ for subset in config['subsets']:
         group_recs = recs_by_name[name]
         molecules = mols_by_name[name]
         qcvars = [r.extras['qcvars'] for r in group_recs]
+        smiles = molecules[0].extras['canonical_isomeric_explicit_hydrogen_mapped_smiles']
+        rdmol = Chem.MolFromSmiles(smiles, sanitize=False)
+        ref_energy = sum(atom_energy[(atom.GetSymbol(), atom.GetFormalCharge())] for atom in rdmol.GetAtoms())
         group = outputfile.create_group(name)
         group.create_dataset('subset', data=[subset], dtype=h5py.string_dtype())
-        group.create_dataset('smiles', data=[molecules[0].extras['canonical_isomeric_explicit_hydrogen_mapped_smiles']], dtype=h5py.string_dtype())
+        group.create_dataset('smiles', data=[smiles], dtype=h5py.string_dtype())
         group.create_dataset("atomic_numbers", data=molecules[0].atomic_numbers, dtype=np.int16)
-        conformations = group.create_dataset('conformations', data=np.array([m.geometry for m in molecules]), dtype=np.float32)
+        group.create_dataset('conformations', data=np.array([m.geometry for m in molecules]), dtype=np.float32)
+        group.create_dataset('formation_energy', data=np.array([vars['DFT TOTAL ENERGY']-ref_energy for vars in qcvars]), dtype=np.float32)
         for value in config['values']:
             key = value.lower().replace(' ', '_')
             try:
