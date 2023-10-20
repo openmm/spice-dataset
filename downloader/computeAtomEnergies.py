4e40fbb8c3c41762f378e2964e3edb4022a801d9
Peter Eastman <peastman@stanford.edu>
2023-10-20 11:12:26 -0700

    Downloader supports B and Si (#86)

--- downloader/computeAtomEnergies.py
+++ downloader/computeAtomEnergies.py
@@ -4,8 +4,8 @@ from openmm.app import element
 # This script uses Psi4 to compute the per-atom reference energies that appear in the downloader script.
 # It is included here in case we want to add more elements in the future.
 
-charges = {'Br': (-1, 0), 'C': (-1, 0, 1), 'Ca': (2,), 'Cl': (-1, 0), 'F': (-1, 0), 'H': (-1, 0, 1), 'I': (-1, 0),
-           'K': (1,), 'Li': (1,), 'Mg': (2,), 'N': (-1, 0, 1), 'Na': (1,), 'O': (-1, 0, 1), 'P': (0, 1), 'S': (-1, 0, 1)}
+charges = {'B': (-1, 0, 1), 'Br': (-1, 0), 'C': (-1, 0, 1), 'Ca': (2,), 'Cl': (-1, 0), 'F': (-1, 0), 'H': (-1, 0, 1), 'I': (-1, 0),
+           'K': (1,), 'Li': (1,), 'Mg': (2,), 'N': (-1, 0, 1), 'Na': (1,), 'O': (-1, 0, 1), 'P': (0, 1), 'S': (-1, 0, 1), 'Si': (-1, 0, 1)}
 
 psi4.set_options({'reference': 'uhf'})
 energies = {}
